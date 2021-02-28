96f6c03ec89a8734e65e74b3a1a7a04a97cc8cea
dhruv <dhruv.g123@gmail.com>
2021-02-28 15:16:14 -0500

    all CRUD opperations implemented

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -10,8 +10,33 @@ module.exports =  function(app, db) {
             } else {
                 res.send(item);
             } 
-        })
-    })
+        });
+    });
+
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id
+        const details = {'_id': new ObjectID(id) };
+        db.collection('notes').remove(details, (err, item) => {
+            if(err) {
+                res.send({ 'error': 'An error has occured'});
+            } else {
+                res.send('Note ' + id + ' deleted');
+            } 
+        });
+    });
+
+    app.put('/notes/:id', (req, res) => {
+        const id = req.params.id
+        const details = {'_id': new ObjectID(id) };
+        const note = { text: req.body.body, title: req.body.title};
+        db.collection('notes').update(details, note, (err, item) => {
+            if(err) {
+                res.send({ 'error': 'An error has occured'});
+            } else {
+                res.send(item);
+            } 
+        });
+    });
     
     app.post('/notes', (req, res) => {
         const note = { text: req.body.body, title: req.body.title};
