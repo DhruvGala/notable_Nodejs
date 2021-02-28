76caa180338409b6175038de2d7a897ab80ddf75
dhruv <dhruv.g123@gmail.com>
2021-02-28 14:57:43 -0500

    mongo setup and create

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,12 @@
 module.exports =  function(app, db) {
     app.post('/notes', (req, res) => {
-        //create the note here
-        console.log(req.body)
-        res.send('Hello')
+        const note = { text: req.body.body, title: req.body.title};
+        db.collection('notes').insert(note, (err, results) => {
+            if(err) {
+                res.send({ 'error': 'An error has occured'});
+            } else {
+                res.send(results.ops[0]);
+            }
+        });
     });
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 const app = express();
 
@@ -8,8 +9,11 @@ const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended:true}))
 
-require('./app/routes')(app, {});
+MongoClient.connect(db.url, (err, database) => {
+    if(err) return console.log(err)
 
-app.listen(port, () => {
-    console.log("live on port " + port);
+    require('./app/routes')(app, database);
+    app.listen(port, () => {
+        console.log("live on port " + port);
+    })
 })
